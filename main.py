68460a1372e36b0acdc5f85e65c9ce0417a0fa66
Eugene Elantsev <eugene_el@mail.ru>
2022-05-20 19:16:58 +0500

    try bot

---
+++ main.py
@@ -0,0 +1,48 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot('asdasdqwdqwdd33223r23r23f23f2')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    mess = f'Hello <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    markup = types.ReplyKeyboardMarkup()
+    website = types.KeyboardButton()
+
+    markup.add(types.InlineKeyboardButton('Пумц на Яндекс', url='http://www.yandex.ru'))
+    bot.send_message(message.chat.id, '<u>Идите на яндекс</u>', parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(commands=['website'])
+def website(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Пумц на Яндекс', url = 'http://www.yandex.ru'))
+    bot.send_message(message.chat.id, '<u>Идите на яндекс</u>', parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def get_user_text(message):
+    if message.text == 'hello':
+        bot.send_message(message.chat.id, 'И тебе хеллоу', parse_mode='html')
+    elif message.text == 'id':
+        bot.send_message(message.chat.id, f'Твой ID: {message.from_user.id}', parse_mode='html')
+    elif message.text == 'photo':
+        photo = open('its_worked.gif', 'rb')
+        bot.send_photo(message.chat.id, photo)
+    else:
+        bot.send_message(message.chat.id, 'Моя твоя непонимайт', parse_mode='html')
+        #bot.send_message(message.chat.id, message, parse_mode='html')
+
+
+@bot.message_handler(content_types=['sticker'])
+def get_user_sticker(message):
+    bot.send_message(message.chat.id, '<u>Гуд sticker я я</u>', parse_mode='html')
+
+
+bot.polling(none_stop=True)
